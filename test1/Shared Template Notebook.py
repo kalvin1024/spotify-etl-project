a42f7ff74e841379820117252055b4e8d5ffe724
kalvin1024 <kalvin1024>
2024-12-19 01:37:50 +0000

    first commit
    hello

---
+++ test1/Shared Template Notebook.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+print("Hello Daniel")
+print("Hello Databricks and 12345678")
+
+# COMMAND ----------
+
+# Daniel -- 
+# TODO: Spotify API called from here and return JSON
+# TODO: Parse JSON to Sparks df 
+# Reference: https://developer.spotify.com/documentation/web-api/reference/get-several-tracks
+
+# Kalvin -- 
+# TODO: Create a database storage for data deposit (S3 or others)
+# TODO: Write the df Daniel created here to the database via databricks interface and can fetch it (possibly on my AWS)
+
+# Angel --
+# TODO: Please think about how to design entity relation schemas for Spotify data: star or snowflake?? Either should be ok, I am thinking of ER/Star
+# Reference: https://developer.spotify.com/documentation/web-api/reference/
+# e.g. Tracks, Artists, Playlists, Albums, Genre, Users, etc.
+
